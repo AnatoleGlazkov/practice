ad6d6e878de1eb2351fa589935adfeab81c02ee9
aaglazkov <Anatoly.Glazkov@russianpost.ru>
2023-05-15 19:05:55 +0600

    1071. Greatest Common Divisor of Strings

--- src/test/java/algorithms/string/GreatestCommonDivisorOfStringsTest.java
+++ src/test/java/algorithms/string/GreatestCommonDivisorOfStringsTest.java
@@ -22,7 +22,8 @@ public class GreatestCommonDivisorOfStringsTest {
             Arguments.of("LEET", "CODE", ""),
             Arguments.of("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXX"),
             Arguments.of("CXTXNCXTXNCXTXNCXTXNCXTXN", "CXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXN", "CXTXN"),
-            Arguments.of("ABCDEF", "ABC", "")
+            Arguments.of("ABCDEF", "ABC", ""),
+            Arguments.of("ABABABAB", "ABAB", "ABAB")
         );
     }
 
@@ -34,48 +35,19 @@ public class GreatestCommonDivisorOfStringsTest {
         String str2,
         String expected
     ) {
-
-//        StringBuilder result = new StringBuilder();
-//
-//        for (int i = 0; i < str1.length(); i++) {
-//            char ch1 = str1.charAt(i);
-//
-//            if (str2.length() > i && ch1 == str2.charAt(i)) {
-//                result.append(ch1);
-//
-//                String[] split = str1.split(result.toString());
-//                if (split.length == 0) {
-//                    break;
-//                }
-//            } else {
-//                result.setLength(0);
-//                break;
-//            }
-//        }
-
-        Assertions.assertEquals(expected, gcdOfStrings(str1, str2));
-    }
-
-
-    public boolean valid(String str1, String str2, int k) {
+        String result = "";
         int len1 = str1.length(), len2 = str2.length();
-        if (len1 % k > 0 || len2 % k > 0) {
-            return false;
-        } else {
-            String base = str1.substring(0, k);
-            return str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty();
-        }
-    }
 
-
-    public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length(), len2 = str2.length();
         for (int i = Math.min(len1, len2); i >= 1; --i) {
-            if (valid(str1, str2, i)) {
-                return str1.substring(0, i);
+            if (len1 % i == 0 || len2 % i == 0) {
+                String base = str1.substring(0, i);
+                if (str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty()) {
+                    result = base;
+                    break;
+                }
             }
         }
-        return "";
-    }
 
+        Assertions.assertEquals(expected, result);
+    }
 }
