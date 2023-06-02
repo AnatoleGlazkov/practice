9090f02ef261a40bfea19ceb6e7a6616f38f2f82
aaglazkov <Anatoly.Glazkov@russianpost.ru>
2023-06-02 15:43:44 +0600

    872. Leaf-Similar Trees

--- src/test/java/utils/node/item/TreeNode.java
+++ src/test/java/utils/node/item/TreeNode.java
@@ -7,9 +7,9 @@ import utils.node.Node;
 /** The type TreeNode */
 public class TreeNode implements Node {
 
-    public final int val;
-    public final TreeNode left;
-    public final TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(
         int val,
@@ -19,6 +19,12 @@ public class TreeNode implements Node {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
     }
 
     @Override
